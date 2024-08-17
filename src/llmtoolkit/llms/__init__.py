57dacb7727fbbf17e29e7fb551ba78382253111e
Mykyta Ishchenko <53219125+mykyta-ishchenko@users.noreply.github.com>
2024-08-17 14:37:47 +0300

    Revert "Dev (#1)"
    This reverts commit 3b7765ceb0e975eeffdf504f4b1e950081b74b0d.

--- src/llmtoolkit/llms/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Module for the language models in the LLM-Toolkit library.
-"""
-
-from llmtoolkit.llms.base import BaseLLMModel
-from llmtoolkit.llms.ollama import OllamaLLMModel
-
-__all__ = ["BaseLLMModel", "OllamaLLMModel"]
